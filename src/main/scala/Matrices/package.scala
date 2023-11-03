e56f9311aa215065cdd1d070660a6bbe129e6556
Julian Puyo <112900679+puyojulian@users.noreply.github.com>
2023-11-02 20:56:47 -0500

    Prueba.sc creado

--- src/main/scala/Matrices/package.scala
+++ src/main/scala/Matrices/package.scala
@@ -1,5 +1,35 @@
 import common._
+import scala.collection.parallel.immutable.ParVector
 import scala.util.Random
 package object Matrices {
+  val random = new Random()
+  type Matriz = Vector[Vector[Int]]
 
+  def matrizAlAzar(long: Int, vals: Int): Matriz = {
+    // Crea una matriz de enteros cuadrada de long x long,
+    // con valores aleatorios entre 0 y vals.
+    val v = Vector.fill(long, long){random.nextInt(vals)}
+    v
+  }
+
+  def vectorAlAzar(long: Int, vals: Int): Vector[Int] = {
+    //Crea un vector de enteros de longitud long,
+    // con valores aleatorios entre 0 y vals
+    val v = Vector.fill(long){random.nextInt(vals)}
+    v
+  }
+
+  def transpuesta(m: Matriz): Matriz = {
+    val l = m.length
+    Vector.tabulate(l, l)((i, j) => m(j)(i))
+  }
+
+  def prodPunto(v1: Vector[Int], v2: Vector[Int]): Int = {
+    (v1 zip v2).map({case (i, j) => (i*j)}).sum
+  }
+
+  def prodPuntoParD(v1: ParVector[Int], v2: ParVector[Int]): Int = {
+    // A ser usada en el punto 1.5
+    (v1 zip v2).map({case (i, j) => (i*j)}).sum
+  }
 }
